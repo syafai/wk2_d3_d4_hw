e07e7222fd84f42aee230f4f7987c7578af72e65
Saraa Yafai <yafaisaraa@gmail.com>
2023-06-02 01:08:08 -0400

    first commit

---
+++ hw_2.py
@@ -0,0 +1,9 @@
+
+
+def circle():
+    print("Lets calculate the circumference of a circle")
+    rad = int(input("Enter the radius of the circle "))
+    circum = 2*3.14*rad
+    return f"The circumference of a circle is {circum}"
+   
+    
---
+++ hw_module.py
@@ -0,0 +1,13 @@
+
+
+def sq_ft():
+    print("Lets calculate the square footage of your house")
+    lenth = int(input("Enter the lenth of the house "))
+    width = int(input("Enter the width of the house "))
+    area = lenth * width
+    return f"This house is {area} square feet"
+
+
+
+   
+    
